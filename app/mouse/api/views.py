d3374bb89f33f0feb07ab23af528a5c94f06d35f
Yuto Hayashi <y_hayashi@alta.co.jp>
2021-09-06 10:04:10 +0900

    20210906進捗

--- app/mouse/api/views.py
+++ app/mouse/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index( request ):
+    return render( request, 'api/index.html', {
+        'title': 'API view',
+        'message': 'api template page. aaaaaaaaaaaaa',
+    } )
+
+def next( request ):
+    return HttpResponse( 'next' )
