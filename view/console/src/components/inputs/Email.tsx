74f886f104953cbe2300e8ad3fffae3ebc79c588
YutoHayashi <y_hayashi@alta.co.jp>
2021-10-16 19:15:24 +0900

    進捗
    at 外出先

--- view/console/src/components/inputs/Email.tsx
+++ view/console/src/components/inputs/Email.tsx
@@ -1,19 +1,62 @@
 import React from 'react';
+import { Mdi } from '../utils/Mdi';
 import { Required } from './Required';
-interface Props {
+import { InputAttribute } from './InputAttributes';
+interface Props extends InputAttribute {
     title: string;
+    name: string;
     description?: string;
+    default?: string;
+    color?: string;
     required?: boolean;
-    placeholder?: string;
-    autocomplete?: boolean;
+    maxLength?: number;
+    minLength?: number;
+    onError?: ( value: string | undefined ) => void;
 }
-interface States {  }
-export class Text extends React.Component<Props, States> {
+interface States {
+    value: string | undefined;
+    errorMessage: string;
+}
+export class Email extends React.Component<Props, States> {
+    private requiredMessage: string = 'This is a required field.';
+    private emailMessage: string = 'This is not an email address.';
     public constructor( props: Props ) {
         super( props );
+        this.state = {
+            value: this.props.default,
+            errorMessage: '',
+        };
+    }
+    private required( value: string | undefined ): true | string {
+        const result: boolean = this.props.required ? !!value : true;
+        return result || this.requiredMessage;
+    }
+    private email( value: string | undefined ): true | string {
+        const result: boolean = /https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+/.test( value ? value : '' );
+        return result || this.emailMessage;
+    }
+    public isValid( value?: string ): boolean {
+        const errorMessage: string = [
+            this.required( value ),
+            this.email( value ),
+        ].map( _ => typeof _ === 'string' ? _ : '' ).reduce( ( p, c ) => `${ p !== '' ? `${ p } ` : '' }${ c }`, '' );
+        this.setState( { ...this.state, ...{ errorMessage, } } );
+        return errorMessage === '';
+    }
+    private onChange(  ): React.ChangeEventHandler<HTMLInputElement> {
+        const { onError = value => null } = this.props;
+        let tID: ReturnType<typeof setTimeout>;
+        return e => {
+            if ( tID ) clearTimeout( tID );
+            const value = e.target.value;
+            tID = setTimeout( (  ) => {
+                if ( this.isValid( value ) ) onError( value );
+            }, 500 );
+        };
     }
     public render(  ) {
-        const { title, description = '', required = false, placeholder = '', autocomplete = false, } = this.props;
+        const { title, name, description = '', color = 'blue-500', required = true, placeholder, autocomplete = false, autofocus = false, readonly = false, maxLength, minLength, } = this.props;
+        const { value, errorMessage } = this.state;
         return (
             <label className={ `inline-block my-5 w-full cursor-pointer` }>
                 <p className={ `text-sm font-bold` }>
@@ -21,7 +64,8 @@ export class Text extends React.Component<Props, States> {
                     { required ? <Required /> : null }
                 </p>
                 <small className={ `text-gray-500 text-sm` }>{ description }</small>
-                <input type='email' placeholder={ placeholder !== '' ? placeholder : `${ title }` } className={ `text-base text-gray-500 font-bold w-full cursor-pointer mt-1 outline-none focus:outline-none focus:ring-0 border-t-0 border-r-0 border-l-0 border-transparent focus:border-blue-500 bg-transparent` } { ...{ required } } { ...{ autoComplete: autocomplete ? 'on' : 'off' } } />
+                <small className={ `text-red-500` }>{ errorMessage !== '' ? <><Mdi icon='alert-circle' />{ errorMessage }</> : null }</small>
+                <input type='email' { ...{ name, required, } } placeholder={ placeholder !== '' ? placeholder : `${ title }` } className={ `text-base text-gray-500 font-bold w-full cursor-pointer mt-1 outline-none focus:outline-none focus:ring-0 border-t-0 border-r-0 border-l-0 border-transparent focus:border-${ color } bg-transparent` } { ...{ autoComplete: autocomplete ? 'on' : 'off', autoFocus: autofocus, maxLength, minLength, readOnly: readonly, value, onChange: this.onChange(  ) } } />
             </label>
         );
     }
--- view/console/src/pages/iam/members/Add.tsx
+++ view/console/src/pages/iam/members/Add.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Member } from '@/layouts/Member';
 import { links } from './links';
 import { Text } from '@/components/inputs/Text';
+import { Email } from '@/components/inputs/Email';
 interface Props {  }
 interface States {  }
 export class Add extends React.Component<Props, States> {
@@ -18,6 +19,7 @@ export class Add extends React.Component<Props, States> {
                             <section className={ `w-full` }>
                                 <h2 className={ `font-bold underline` }>Add Member</h2>
                                 <Text title={ `Name` } name={ `name` } required={ true } autofocus={ true } />
+                                <Email title={ `Email` } name={ 'email' } required={ true } autocomplete={ false } />
                             </section>
                         );
                     },
