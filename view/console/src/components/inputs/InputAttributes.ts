d6a54c7c51edb234520656a7ff9016d78a003e6b
YutoHayashi <y_hayashi@alta.co.jp>
2021-10-16 15:20:41 +0900

    進捗

---
+++ view/console/src/components/inputs/InputAttributes.ts
@@ -0,0 +1,8 @@
+export interface InputAttribute {
+    placeholder?: string;
+    autocomplete?: boolean;
+    autofocus?: boolean;
+    readonly?: boolean;
+    checked?: boolean;
+    disabled?: boolean;
+}
--- view/console/src/components/inputs/Text.tsx
+++ view/console/src/components/inputs/Text.tsx
@@ -1,69 +1,55 @@
 import React from 'react';
 import { Mdi } from '../utils/Mdi';
 import { Required } from './Required';
-import { WithValidator } from './WithValidator';
-interface Props {
+import { InputAttribute } from './InputAttributes';
+interface Props extends InputAttribute {
     title: string;
     name: string;
     description?: string;
-    required?: boolean;
-    placeholder?: string;
-    autocomplete?: boolean;
-    autofocus?: boolean;
-    max?: number;
-    min?: number;
-    readonly?: boolean;
     default?: string;
     color?: string;
-    validation?: Array<{
-        validator: ( newval: string | undefined ) => boolean;
-        message: string;
-    }>;
+    required?: boolean;
+    maxLength?: number;
+    minLength?: number;
     onError?: ( value: string | undefined ) => void;
 }
 interface States {
     value: string | undefined;
     errorMessage: string;
 }
-export class Text extends WithValidator<Props, States> {
-    private text: React.RefObject<HTMLInputElement> = React.createRef(  );
-    private validator: ( newval: string | undefined ) => boolean;
+export class Text extends React.Component<Props, States> {
+    private requiredMessage: string = 'This is a required field.';
     public constructor( props: Props ) {
         super( props );
         this.state = {
             value: this.props.default,
             errorMessage: '',
         };
-        this.validator = newval => {
-            const { required, validation = [  ] } = this.props;
-            return [ ...validation, ...[ this.required( { required, } ), ], ].map( ( { validator, message } ) => {
-                const result = validator( newval );
-                if ( !result ) this.setState( { ...this.state, ...{ errorMessage: this.state.errorMessage + `${ message } `, }, } );
-                return result;
-            } ).reduce( ( p, c ) => p && c );
-        };
     }
-    public isValid(  ): boolean {
-        return this.validator( this.text.current?.value );
+    private required( value: string | undefined ): true | string {
+        const result: boolean = this.props.required ? !!value : true;
+        return result || this.requiredMessage;
     }
-    public error( args: { value: string | undefined } ) {
-        if ( this.props.onError ) this.props.onError( args.value );
+    public isValid( value?: string ): boolean {
+        const errorMessage: string = [
+            this.required( value ),
+        ].map( _ => typeof _ === 'string' ? _ : '' ).reduce( ( p, c ) => `${ p !== '' ? `${ p } ` : '' }${ c }`, '' );
+        this.setState( { ...this.state, ...{ errorMessage, } } );
+        return errorMessage === '';
     }
-    public changeHandlerRegister(  ) {
+    private onChange(  ): React.ChangeEventHandler<HTMLInputElement> {
+        const { onError = value => null } = this.props;
         let tID: ReturnType<typeof setTimeout>;
-        return ( e: React.ChangeEvent<HTMLInputElement> ) => {
+        return e => {
             if ( tID ) clearTimeout( tID );
+            const value = e.target.value;
             tID = setTimeout( (  ) => {
-                const value = this.text.current?.value;
-                this.setState( { ...this.state, ...{ errorMessage: '', }, } );
-                if ( !this.isValid(  ) ) {
-                    this.error( { value, } );
-                }
-            }, 100 );
+                if ( this.isValid( value ) ) onError( value );
+            }, 500 );
         };
     }
     public render(  ) {
-        const { title, name, description = '', required = false, placeholder = '', autocomplete = false, autofocus = false, max = 255, min = 0, readonly = false, color = 'blue-500' } = this.props;
+        const { title, name, description = '', color = 'blue-500', required = true, placeholder, autocomplete = false, autofocus = false, readonly = false, maxLength, minLength, } = this.props;
         const { value, errorMessage } = this.state;
         return (
             <label className={ `inline-block my-5 w-full cursor-pointer` }>
@@ -73,7 +59,7 @@ export class Text extends WithValidator<Props, States> {
                 </p>
                 <small className={ `text-gray-500 text-sm` }>{ description }</small>
                 <small className={ `text-red-500` }>{ errorMessage !== '' ? <><Mdi icon='alert-circle' />{ errorMessage }</> : null }</small>
-                <input ref={ this.text } type='text' { ...{ name, required, } } placeholder={ placeholder !== '' ? placeholder : `${ title }` } className={ `text-base text-gray-500 font-bold w-full cursor-pointer mt-1 outline-none focus:outline-none focus:ring-0 border-t-0 border-r-0 border-l-0 border-transparent focus:border-${ color } bg-transparent` } { ...{ autoComplete: autocomplete ? 'on' : 'off', autoFocus: autofocus, maxLength: max, minLength: min, readOnly: readonly, value, onChange: this.changeHandlerRegister(  ) } } />
+                <input type='text' { ...{ name, required, } } placeholder={ placeholder !== '' ? placeholder : `${ title }` } className={ `text-base text-gray-500 font-bold w-full cursor-pointer mt-1 outline-none focus:outline-none focus:ring-0 border-t-0 border-r-0 border-l-0 border-transparent focus:border-${ color } bg-transparent` } { ...{ autoComplete: autocomplete ? 'on' : 'off', autoFocus: autofocus, maxLength, minLength, readOnly: readonly, value, onChange: this.onChange(  ) } } />
             </label>
         );
     }
